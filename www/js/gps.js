b21d97ba9ff9048e584ee00903a7c08095b00dd3
Kurt Beheydt <kurt@postduif.be>
2018-08-28 10:53:44 +0200

    better background location implementation (i hope)

--- www/js/gps.js
+++ www/js/gps.js
@@ -9,45 +9,54 @@ var gps = {
             desiredAccuracy: 10,
             stationaryRadius: 20,
             distanceFilter: 30,
-            interval: 60000
+            interval: 10000
         });
-
         backgroundGeolocation.start();
     },
     onBackgroundInitSuccess: function (location) {
+        console.log('-----background location success:-----');
+        console.log(location);
         gps.coords = {
             lat: location.latitude,
             lng: location.longitude,
             acc: location.accuracy
         }
-        debug.log(gps.coords);
     },
     onBackgroundInitFailure: function (error) {
         debug.log('background gps failure');
         console.log(error);
     },
-    getLocation: function () {
-        try {
-            navigator.geolocation.getCurrentPosition(gps.onSuccess, app.onError);
-        } catch (exception) {
-            debug.log('gps fetch failed', 'error');
-            console.log(exception);
-        }
-    },
-    onSuccess: function (position) {
-        console.log(position.coords);
-        $('#geolocation').html('Latitude: ' + position.coords.latitude + '<br />' +
-            'Longitude: ' + position.coords.longitude + '<br />' +
-            'Altitude: ' + position.coords.altitude + '<br />' +
-            'Accuracy: ' + position.coords.accuracy + '<br />' +
-            'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
-            'Heading: ' + position.coords.heading + '<br />' +
-            'Speed: ' + position.coords.speed + '<br />');
+    getLocation: function (onSuccessCallback) {
+        backgroundGeolocation.getLocations(function (locations) {
+                gps.coords = locations[locations.length - 1];
+                gps.coords.lat = gps.coords.latitude;
+                gps.coords.lng = gps.coords.longitude;
+                gps.coords.acc = gps.coords.accuracy;
+                gps.showLocation();
+                gps.onSuccess();
 
-        gps.coords = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-            acc: position.coords.accuracy
-        }
+                if (typeof (onSuccessCallback) === 'function') {
+                    onSuccessCallback();
+                }
+            }, function (error) {
+                debug.log('background gps getLocations failure');
+                console.log(error);
+            }
+        );
+    },
+    showLocation: function () {
+        console.log('-----location showed:-----');
+        console.log(gps.coords);
+        $('#geolocation').html('Latitude: ' + gps.coords.latitude + '<br />' +
+            'Longitude: ' + gps.coords.longitude + '<br />' +
+            'Altitude: ' + gps.coords.altitude + '<br />' +
+            'Accuracy: ' + gps.coords.accuracy + '<br />' +
+            'Altitude Accuracy: ' + gps.coords.altitudeAccuracy + '<br />' +
+            'Heading: ' + gps.coords.heading + '<br />' +
+            'Speed: ' + gps.coords.speed + '<br />');
+    },
+    onSuccess: function () {
+        console.log('-----getLocations location success:-----');
+        console.log(gps.coords);
     }
 }
